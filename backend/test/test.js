f954fa0807fb26f2b7be931a7c2fab606a53cb7d
stevensam0109 <steven.sam.0109@outlook.com>
2021-03-03 16:46:16 +0700

    unit test login and view data agama

--- backend/test/test.js
+++ backend/test/test.js
@@ -1,33 +1,62 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const expect = chai.expect;
-const app = require('./../index');
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let expect = chai.expect;
+let app = require('./../index');
 
 chai.use(chaiHttp);
 
-describe('API Agama Testing', () => {
+let token = '';
 
-    let token = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBlcm1hZGkiLCJ1c2VySWQiOiJjZmU3MGI3YS1iMDg3LTQ4ZmYtODM5Yy05MGFkNDYyMjU5YzIiLCJpYXQiOjE2MTQ3MzgyMzUsImV4cCI6MTYxNDgyNDYzNX0.i0AqZcgG-on4AvWSYFQLmozIEDzvTFCfxfPb1lMuu00;
-    let username = null;
-    let password = null;
+describe('Test Login User', () => { 
 
-    before(function(done) {
-        chai.request(app)
-        .post('/login')
-        .send({ "permadi": username, "permadi": password })
-        .end(function(err, res) {
-            token2 = res.body.token;
-            done();
-        });
-    });
+    it('User should be in database', (done) => {
+        chai.request(app).post('/login')
+            .send({
+                'username': 'permadi',
+                'password': 'permadi'
+            })
+            .end((err, res) => {
+                // console.log(res.body);
+                // console.log(res.body.token);
+                expect(res.body).to.have.property('token');
+                token = res.body.token;
+                expect(err).to.be.null,
+                expect(res).to.have.be.status(200)
+                done();
+            })
+    })
 
-    it('GET All Agama', (done) => {
-        // chai.request(app).get('/agama/view').set('Authorization', 'Bearer ' + token).end((err, res) => {   
-        // chai.request(app).get('/agama/view').set(token,{ type: 'bearer' }).end((err, res) => {         
-        chai.request(app).get('/agama/view').set({ "Authorization": `Bearer ${token}` }).end((err, res) => {  
+    it('User not in database', (done) => {
+        chai.request(app).post('/login')
+            .send({
+                'username': '.',
+                'password': '.'
+            })
+            .end((err, res) => {
+                expect(err).to.be.null,
+                expect(res).to.have.be.status(401)
+                done();
+            })
+    })
+
+});
+
+describe('Test API Agama', () => {
+
+    it('GET All Data Agama', (done) => {           
+        chai.request(app).get('/agama/view').set('Authorization', 'Bearer' + token).end((err, res) => {  
             expect(err).to.be.null,
             expect(res).to.have.be.status(200)
             done();
         })
     })
+
+    it('GET Agama by ID (belum solved)', (done) => {
+        chai.request(app).get('/agama/view').set('Authorization', 'Bearer' + token).end((err, res) => {  
+            expect(err).to.be.null,
+            expect(res).to.have.be.status(200)
+            done();
+        })
+    })
+
 });
